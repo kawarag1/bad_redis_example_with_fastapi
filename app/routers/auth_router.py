6013ec29b4667e7e0a9d1e4a8df3c8d5fa294b44
kawarag1 <dima.kretinin.05@mail.ru>
2026-03-06 19:08:21 +0700

    добавление получения информации о девайсе через depends

--- app/routers/auth_router.py
+++ app/routers/auth_router.py
@@ -5,7 +5,7 @@ from app.database.connector import get_session
 from app.schema.request.user_register import UserRegister
 from app.schema.response.access_token import AccessToken
 from app.schema.response.user_read import UserRead
-from app.security.oauth import get_current_user
+from app.security.oauth import get_current_user, get_device_dict as get_device
 from app.services.user_service import UserService
 
 router = APIRouter(
@@ -13,15 +13,15 @@ router = APIRouter(
 )
 
 @router.post("/register", response_model=AccessToken)
-async def register_user(user_register: UserRegister, session: AsyncSession = Depends(get_session)):
-    return await UserService(session).register_user(user_register)
+async def register_user(user_register: UserRegister, device_info = Depends(get_device), session: AsyncSession = Depends(get_session)):
+    return await UserService(session).register_user(user_register, device_info)
 
 
 @router.post("/authorize", response_model=AccessToken)
 async def authorize_user(
-    username: str = Form(...), password: str = Form(...), session: AsyncSession = Depends(get_session)
+    username: str = Form(...), password: str = Form(...), device_info = Depends(get_device), session: AsyncSession = Depends(get_session)
 ):
-    return await UserService(session).authorize_user(username, password)
+    return await UserService(session).authorize_user(username, password, device_info)
 
 
 @router.get("/blacklist")
