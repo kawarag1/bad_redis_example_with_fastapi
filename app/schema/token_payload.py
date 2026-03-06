f069a04b771d0c7c14b9bb76ae84cdbbec07cafa
kawarag1 <dima.kretinin.05@mail.ru>
2026-03-06 19:07:08 +0700

    новые поля для устройства

--- app/schema/token_payload.py
+++ app/schema/token_payload.py
@@ -6,6 +6,10 @@ from pydantic import BaseModel
 class TokenPayload(BaseModel):
     iss: str | None = None
     sub: str
+    device_type: str
+    device_os: str
+    device_browser: str
+    device_model: str 
     aud: str | None = None
     exp: datetime | None = None
     nbf: int | None = None
